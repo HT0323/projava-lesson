6bd653abbad8e53402da40395ab38435e8ea889a
HaraT0323 <alra9si916323@gmail.com>
2023-01-12 17:47:07 +0900

    for文

---
+++ src/main/java/projava/ForSample.java
@@ -0,0 +1,40 @@
+package projava;
+
+public class ForSample {
+    public static void main(String[] args) {
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 0; i < 5; i += 2) {
+//            System.out.println(i);
+//        }
+
+//        int i = 0;
+//        while (i < 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        for (int i = 0; i < 5; i++) {
+//            if (i == 2) {
+//                System.out.println("skip");
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+//        for (int i = 0; i < 5; i++) {
+//            if (i == 2) {
+//                System.out.println("finsh");
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.print("0".repeat(i));
+            System.out.println();
+        }
+    }
+}
